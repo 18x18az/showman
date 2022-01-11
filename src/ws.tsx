5f364d3df1beedf6fbc92aa60789679fa3d07b93
Alec Miller <alecmichaelmiller@gmail.com>
2022-01-11 16:09:32 -0700

    correctly generate socket url

--- src/ws.tsx
+++ src/ws.tsx
@@ -1,5 +1,8 @@
 import { Websocket } from "@18x18az/ouija";
 
-const talos_url = "ws://localhost:1270"
+const server = window.location.hostname;
+const port = '1270'
+
+const talos_url = `ws://${server}:${port}`
 
 export const talos = new Websocket(talos_url);
