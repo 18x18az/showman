f18d1dace304b65a26133753b3cd8f8a6ae8c780
Alec Miller <alecmichaelmiller@gmail.com>
2022-01-13 17:03:03 -0700

    css changes

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <link rel="preconnect" href="https://fonts.gstatic.com"/>
-    <link href="https://fonts.googleapis.com/css2?family=Electrolize&family=Orbitron:wght@400;748&display=swap" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible&family=Electrolize&family=Orbitron:wght@400;748&display=swap" rel="stylesheet"/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
