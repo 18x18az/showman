a85175899f9dffb6019bd02f568d3460f8304cc2
Alec Miller <alecmichaelmiller@gmail.com>
2023-01-02 16:33:21 -0700

    added inspection display

---
+++ src/pages/pit/Inspection/Inspection.tsx
@@ -0,0 +1,25 @@
+import { IInspectionStatus, ITeams } from "@18x18az/rosetta";
+import { Fragment } from "react"
+import { TeamList } from "./TeamList";
+
+interface InspectionProps {
+    teams: ITeams
+    inspectionState: IInspectionStatus | null
+}
+
+export const Inspection = (props: InspectionProps) => {
+    console.log("Inspection")
+    if(props.inspectionState === null){
+        return <Fragment/>
+    }
+
+    console.log("has state");
+
+    return(
+        <div className="inspection">
+            <TeamList title="NOT STARTED" teams={props.inspectionState.notStarted} teamData={props.teams}/>
+            <TeamList title="PARTIAL" teams={props.inspectionState.partial} teamData={props.teams}/>
+            <TeamList title="COMPLETE" teams={props.inspectionState.inspected} teamData={props.teams}/>
+        </div>
+    )
+};
