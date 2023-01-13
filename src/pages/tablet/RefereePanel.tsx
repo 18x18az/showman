2a81cb8e21a57c447476e86a2fb672e0c34c9198
Alec Miller <alecmichaelmiller@gmail.com>
2023-01-12 20:30:02 -0700

    improved field control

--- src/pages/tablet/RefereePanel.tsx
+++ src/pages/tablet/RefereePanel.tsx
@@ -1,7 +1,7 @@
-import { IFieldInfo, IFieldState, IMatchList, IPath, ITeams, IAllianceTeams } from "@18x18az/rosetta";
+import { IFieldInfo, IFieldState, IMatchList, IPath, ITeams, IAllianceTeams, FIELD_COMMAND, FIELD_CONTROL, REF_COMMAND, MATCH_STAGE } from "@18x18az/rosetta";
 import { Component } from "react";
 import { talos } from "../../ws";
-import { determineMatch, isPreMatch, isInMatch, isMatchPaused, isMatchEnded} from "../../utils/Field";
+import { determineMatch, isInMatch, isMatchEnded } from "../../utils/Field";
 import { makeClockText, makeMatchName } from "../../utils/TextGenerator";
 
 interface RefereeProps {
@@ -14,14 +14,7 @@ interface RefereeProps {
 interface RefereeState {
     field: IFieldState | null
     fields: Array<IFieldInfo> | null
-    disableQueue: number // 0: unlocked; 1: locked; 2: passed locked phase
-    disableStart: boolean
-}
-
-enum QUEUE_LOCK_STATUS { 
-    UNLOCKED = 0,
-    LOCKED = 1,
-    POSTLOCKED = 2
+    matchStage: MATCH_STAGE | null
 }
 
 export class RefereePanel extends Component<RefereeProps, RefereeState> {
@@ -29,32 +22,26 @@ export class RefereePanel extends Component<RefereeProps, RefereeState> {
         super(props);
         talos.get(['field'])
         talos.get(['fields'])
+        talos.get(['matchStage'])
 
         this.onStartClick = this.onStartClick.bind(this);
-        this.onQueueClick = this.onQueueClick.bind(this);
+        this.onShowClick = this.onShowClick.bind(this);
 
         this.state = {
-            disableQueue: QUEUE_LOCK_STATUS.UNLOCKED,
-            disableStart: false,
+            matchStage: null,
             field: null,
             fields: null
         }
     }
 
-    onQueueClick() {
-        // lock out queue button after queuing the next match
-        this.setState({ disableQueue: QUEUE_LOCK_STATUS.LOCKED });
-        setTimeout(() => this.setState({ disableQueue: QUEUE_LOCK_STATUS.UNLOCKED }), 1000);
-
-        // send queue action
-        talos.post(["fieldcontrol"], {"type": "QUEUE", "action": "NextMatch", "fieldID": "brrr"});
+    onShowClick() {
+        talos.post(["refCommand"], REF_COMMAND.DISPLAY);
     }
 
     onStartClick() {
         // TODO: since we lock the button in the HTML, is this if statement needed?
         if (this.state.field && (this.state.field.timeRemaining !== 0 || this.state.field.control === "PAUSED")) {
-            let fieldID = this.state.field.field;
-            talos.post(["fieldcontrol"], {"type": "MATCH", "action": "start", "fieldID": fieldID});
+            talos.post(["refCommand"], REF_COMMAND.START);
         }
     }
 
@@ -62,60 +49,47 @@ export class RefereePanel extends Component<RefereeProps, RefereeState> {
         if (nextProps.lastMessagePath) {
             const route = nextProps.lastMessagePath[0];
             if (route === "field") {
-                let queueLock: number = QUEUE_LOCK_STATUS.UNLOCKED;
-                if (!isMatchEnded(nextProps.lastMessageBody)) {
-                    queueLock = QUEUE_LOCK_STATUS.LOCKED;
-                }
-
-                if (prevState.disableQueue === QUEUE_LOCK_STATUS.POSTLOCKED) {
-                    queueLock = QUEUE_LOCK_STATUS.POSTLOCKED;
-                }
                 return ({
                     field: nextProps.lastMessageBody,
-                    disableStart: isInMatch(nextProps.lastMessageBody) || isMatchEnded(nextProps.lastMessageBody),
-                    disableQueue: queueLock
+                    disableStart: isInMatch(nextProps.lastMessageBody) || isMatchEnded(nextProps.lastMessageBody)
                 })
             }
             else if (route === "fields") {
-                return({
+                return ({
                     fields: nextProps.lastMessageBody
+                })
+            }
+            else if (route === "matchStage") {
+                return ({
+                    matchStage: nextProps.lastMessageBody
                 })
             }
         }
 
         return null;
     }
-    
-    componentDidUpdate(prevProps: Readonly<RefereeProps>, prevState: Readonly<RefereeState>, snapshot?: any): void {
-        if (this.state.field) {
-            if (isMatchEnded(this.state.field) && prevState.disableQueue !== QUEUE_LOCK_STATUS.LOCKED) {
-                console.log("no need to disable queue bc already disabled")
-                return;
-            }
-            if (isMatchEnded(this.state.field)) {
-                console.log("match ended!");
-                this.setState({
-                    disableQueue: QUEUE_LOCK_STATUS.POSTLOCKED
-                })
-                setTimeout(() => {
-                    this.setState({
-                        disableQueue: QUEUE_LOCK_STATUS.UNLOCKED
-                    });
-                }, 10000);
-            }
-        }
-    }
 
     render() {
-        let fieldName = "field name";
-        let matchName = "match name";
-        let control = "mode";
-        let time = "time";
-        let redTeam1 = "RED1", redTeam2 = "RED2", blueTeam1 = "BLUE1", blueTeam2 = "BLUE2";
-        let queueLock: boolean = this.state.disableQueue !== QUEUE_LOCK_STATUS.UNLOCKED ? true : false;
-        let queueButtonName = queueLock ? "LOCKED" : "QUEUE NEXT MATCH";
-        let controlButtonName = this.state.disableStart ? "LOCKED" : "START MATCH";
-        if (this.state && this.state.fields && this.state.field && this.props.teams && this.props.matches) {
+        const stage = this.state.matchStage;
+        console.log("Here");
+        console.log(stage);
+        if (this.state && this.state.fields && this.state.field && this.state.matchStage && this.props.teams && this.props.matches) {
+            let showMatchLocked = true;
+            if (stage === MATCH_STAGE.IDLE) {
+                showMatchLocked = false;
+            }
+            let startMatchLocked = true;
+            if (stage === MATCH_STAGE.INTRO || stage === MATCH_STAGE.PAUSED) {
+                startMatchLocked = false;
+            }
+
+            let fieldName = "field name";
+            let matchName = "match name";
+            let control = "mode";
+            let time = "time";
+            let redTeam1 = "RED1", redTeam2 = "RED2", blueTeam1 = "BLUE1", blueTeam2 = "BLUE2";
+            let queueButtonName = showMatchLocked ? "LOCKED" : "SHOW NEXT MATCH";
+            let controlButtonName = startMatchLocked ? "LOCKED" : "START MATCH";
 
             // get field name
             this.state.fields.forEach((field) => {
@@ -140,26 +114,28 @@ export class RefereePanel extends Component<RefereeProps, RefereeState> {
                 blueTeam1 = this.props.teams[(match.blue as IAllianceTeams).team1].number;
                 blueTeam2 = this.props.teams[(match.blue as IAllianceTeams).team2].number;
             }
+
+            return (
+                <div className="referee">
+                    <h1 className="matchtitle">{matchName} - {fieldName}</h1>
+                    <h2>{control} - {time}</h2>
+                    <button className="button" onClick={this.onShowClick} disabled={showMatchLocked}>
+                        {queueButtonName}</button>
+                    <br></br>
+                    <button className="button" onClick={this.onStartClick} disabled={startMatchLocked}>
+                        {controlButtonName}</button>
+                    <div className="redteams">
+                        <p>{redTeam1}</p>
+                        <p>{redTeam2}</p>
+                    </div>
+                    <div className="blueteams">
+                        <p>{blueTeam1}</p>
+                        <p>{blueTeam2}</p>
+                    </div>
+                </div>
+            )
+        } else {
+            return <div></div>
         }
-        
-        return (
-            <div className="referee">
-                <h1 className="matchtitle">{matchName} - {fieldName}</h1>
-                <h2>{control} - {time}</h2>
-                <button className="button" onClick={this.onQueueClick} disabled={queueLock}>
-                    {queueButtonName}</button>
-                <br></br>
-                <button className="button" onClick={this.onStartClick} disabled={this.state.disableStart}>
-                    {controlButtonName}</button>
-                <div className="redteams">
-                    <p>{redTeam1}</p>
-                    <p>{redTeam2}</p>
-                </div>
-                <div className="blueteams">
-                    <p>{blueTeam1}</p>
-                    <p>{blueTeam2}</p>
-                </div>
-            </div>
-        )
     }
 }
