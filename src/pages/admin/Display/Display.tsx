5e74d9625b01c539a370d2f71cbdc5489c4770c4
Alec Miller <alecmichaelmiller@gmail.com>
2023-02-20 16:34:38 -0700

    some changes

--- src/pages/admin/Display/Display.tsx
+++ src/pages/admin/Display/Display.tsx
@@ -17,14 +17,53 @@ function showNone() {
     talos.post(['display'], DISPLAY_STATE.NONE);
 }
 
+function transition(transition: string){
+    talos.post(['director', 'transition'], transition);
+}
+
+function sting(){
+    transition('sting');
+}
+
+function cut(){
+    transition('cut');
+}
+
+function overlay(overlay: string){
+    talos.post(['director', 'overlay'], overlay);
+}
+
+function clean(){
+    overlay('clean');
+}
+
+function audience(){
+    overlay('audience');
+}
+
+function timer(){
+    overlay('timer');
+}
+
 export const Display = () => {
     document.title = "Display Control"
-    return(
+    return (
         <div className="displayControl">
-            <button onClick={showUpcoming}>Upcoming</button>
-            <button onClick={showScore}>Score</button>
-            <button onClick={showNone}>None</button>
-            <button onClick={showOther}>Other</button>
+            <div>
+                <button onClick={showUpcoming}>Upcoming</button>
+                <button onClick={showScore}>Score</button>
+                <button onClick={showNone}>None</button>
+                <button onClick={showOther}>Other</button>
+            </div>
+            <div>
+                <button onClick={sting}>Stinger</button>
+                <button onClick={cut}>Cut</button>
+            </div>
+            <div>
+                <button onClick={clean}>Clean</button>
+                <button onClick={timer}>Timer</button>
+                <button onClick={audience}>Audience</button>
+            </div>
         </div>
     );
 }
