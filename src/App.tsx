e3858821526014da4a2afa5f2dba765d15dfe008
Alec Miller <alecmichaelmiller@gmail.com>
2023-06-25 05:48:18 -0700

    feat(talos): talos connection handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Component, Fragment } from "react";
+import { handleTopic } from "./utils/talos";
 
 interface Props {
 
@@ -10,9 +11,17 @@ interface State {
 
 class App extends Component<Props, State> {
   constructor(props: Props) {
-    super(props);
+    super(props)
+
+    handleTopic('stage', this.messageHandler.bind(this))
+    //client.on("message", this.messageHandler.bind(this))
+
     this.state = {
     }
+  }
+
+  messageHandler(topic: string, message: string) {
+    console.log(message)
   }
 
   render() {
---
+++ src/utils/talos.ts
@@ -0,0 +1,17 @@
+import * as mqtt from "mqtt/dist/mqtt";
+
+const host = window.location.hostname; // TODO handle Bifrost
+const mqttBroker = `mqtt://${host}:1819`
+
+const mqttOption = {
+    
+}
+
+export function handleTopic(topic: string, cb: (topic: string, message: string) => void): mqtt.Client {
+    const client = mqtt.connect(mqttBroker, mqttOption)
+    client.on('message', (topic, payload) => {
+        cb(topic, payload.toString())
+    })
+    client.subscribe(topic)
+    return client
+}
