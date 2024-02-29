76c12495c80b1757d1bf0b384fc53dc050e6fd3a
Alec Miller <alecmichaelmiller@gmail.com>
2024-02-28 20:16:34 -0700

    logo reel components

---
+++ src/components/ui/logos/logo-reel-corner.tsx
@@ -0,0 +1,10 @@
+import { LogoReel } from "./logo-reel";
+
+export function LogoReelCorner (): JSX.Element {
+    return (
+      <div className='w-36 h-36 absolute bg-white rounded-md top-6 left-6'>
+        <LogoReel />
+      </div>
+    )
+  }
+  
